1ac18205d2287a04aca0a83d8a36daad0e1c9bf3
nkamath <nagendra.s.kamath@gmail.com>
2018-10-04 21:28:04 +0000

    Added a "Go Back" button

--- server.php
+++ server.php
@@ -86,6 +86,11 @@
         padding: 0;
       }
     </style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -141,7 +146,9 @@
     	<!-- <? //weather_data(); ?> -->
     	<? get_latlon() ?>
     </table>
-
+	<form action="client.php" method="post">
+    	<input type="submit" class="btn btn-danger" value = "Go back">
+    </form>
 </body>
 
 </html>
